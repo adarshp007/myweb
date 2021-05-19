d5d360a8303c6178c5ae8c42234faee50954c655
adarshp007 <adarshmp123@gmail.com>
2021-05-20 01:33:41 +0530

    added content home

--- Accounts/views.py
+++ Accounts/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def home(requset):
     return render(requset,'home.html')
+def content(request):
+    return render(request,'content.html')
